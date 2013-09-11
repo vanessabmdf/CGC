2c632d3c3554efd4373dd4d472b7ee2e35dc5f39
Álvaro Fernando <alvarofernandoms@hotmail.com>
2013-09-11 20:01:20 -0300

    Arrumando script SQL

---
+++ view/header_relatorio.php
@@ -0,0 +1,25 @@
+<?php
+include '../model/Instituicao.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Gest&atilde;o de Contratos GigaCandaga</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Bootstrap -->
+        <link href="../view/css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="../view/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+        <link href="../view/css/style.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <header>
+            <div class="navbar">
+                <div class="navbar-inner">
+                    <ul class="nav">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="#">Sobre</a></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
