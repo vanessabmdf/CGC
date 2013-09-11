bcb7b379398ab74a6acc5ed07a234ae5815adda4
Álvaro Fernando <alvarofernandoms@hotmail.com>
2013-09-11 20:09:38 -0300

    Gambiarra para mostrar todos instituições na tela inicial

--- view/index.php
+++ view/index.php
@@ -1,5 +1,26 @@
-<?php 
+<?php require_once('header.php'); ?>
 
-echo "Teste!"
+<section class="container">
+    <div class="row">
+        <h3>
+            <span itemprop="name">Institui&ccedil;&omacr;es</span>
+        </h3>
 
-?>
+        <?php
+        //require_once('../controller/processaGestao.php') 
+        //selecionando dados da tabela
+        include_once '../model/DAC/conexao.php';
+        $sql = "SELECT * FROM instituicao";
+        $query = mysql_query($sql);
+        while ($sql = mysql_fetch_array($query)) {
+            $id = $sql["idINSTITUICAO"];
+            $nome = $sql["nome"];
+            echo "$nome ID = $id <a href=editarInstituicao.php>Editar</a>";
+            echo "</br>";
+        }
+        ?>
+
+    </div>
+</section>
+
+<?php require_once('footer.php'); ?>
