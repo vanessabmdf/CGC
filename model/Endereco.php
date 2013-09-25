a02afb75c7470e155d62f32f016ca0a25084db2f
Álvaro Fernando <alvarofernandoms@hotmail.com>
2013-09-25 15:36:58 -0300

    Add model de Endereco.php

---
+++ model/Endereco.php
@@ -0,0 +1,55 @@
+<?php
+
+include 'DAC/EnderecoDAC.php';
+
+class Endereco {
+
+    private $endereco;
+    private $bairro;
+    private $cidade;
+    private $estado;
+    private $cep;
+
+    public function getEndereco() {
+        return $this->endereco;
+    }
+
+    public function setEndereco($endereco) {
+        $this->endereco = $endereco;
+    }
+
+    public function getBairro() {
+        return $this->bairro;
+    }
+
+    public function setBairro($bairro) {
+        $this->bairro = $bairro;
+    }
+
+    public function getCidade() {
+        return $this->cidade;
+    }
+
+    public function setCidade($cidade) {
+        $this->cidade = $cidade;
+    }
+
+    public function getEstado() {
+        return $this->estado;
+    }
+
+    public function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    public function getCep() {
+        return $this->cep;
+    }
+
+    public function setCep($cep) {
+        $this->cep = $cep;
+    }
+
+}
+
+?>
