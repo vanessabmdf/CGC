fc12e9d086775d3aa24d501c157af828548a5b35
Álvaro Fernando <alvarofernandoms@hotmail.com>
2013-11-13 16:27:05 -0200

    Pequenas modificações na camada view

--- view/editaInstituicao.php
+++ view/editaInstituicao.php
@@ -1,5 +1,45 @@
 <?php require_once('header.php'); ?>
-<section class="container">
-    <h3>Editar Instituição</h3>
-</section>
+<!-- MAIN CONTAINER -->
+<div id="ja-container" class="control-group">
+    <div class="main clearfix">
+        <div id="ja-mainbody" style="width:100%">
+            <!-- CONTENT -->
+            <div id="ja-main" style="width:100%">
+                <div class="inner clearfix">
+
+
+                    <div id="system-message-container">
+                        <div id="system-message">
+                        </div>
+                    </div>
+
+                    <div id="ja-contentwrap" class="clearfix">
+                        <div id="ja-content" class="column" style="width:100%">
+                            <div id="ja-current-content" class="column" style="width:100%">
+
+                                <div id="ja-content-main" class="ja-content-main clearfix">
+                                    <div class="item-page clearfix">
+                                        <section class="container">
+                                            <h3>Editar Institui&ccedil;&atilde;o</h3>
+
+                                        </section>
+
+                                    </div>                            
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+            </div>
+            <!-- //CONTENT -->
+
+        </div>
+    </div>
+</div>
+<!-- //MAIN CONTAINER -->
 <?php require_once('footer.php'); ?>
